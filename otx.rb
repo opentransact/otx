7cd7455ce38dc877be406603a6b03ab0aa839cf4
Pelle Braendgaard <pelle@stakeventures.com>
2011-06-25 19:38:23 -0400

    tweeks to make it startup

--- otx.rb
+++ otx.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-require 'activerecord'
+require 'active_record'
 require 'active_support/all'
 require 'oauth'
 require 'oauth/consumer'
@@ -7,13 +7,15 @@ require 'oauth/token'
 require 'opentransact'
 
 class Otx < Sinatra::Base
+  ENV["RACK_ENV"] ||= "development"
+
   enable :sessions, :logging
   set :haml, {:format => :html5 }
   set :root, File.dirname(__FILE__)
   set :public, Proc.new { File.join(root, "public") }
 
   configure do
-    dbconfig = YAML.load(File.read('config/database.yml'))
+    dbconfig = YAML.load( File.read( File.join(root, 'config', 'database.yml')))
     ActiveRecord::Base.establish_connection dbconfig[ENV["RACK_ENV"]]
   end
 
